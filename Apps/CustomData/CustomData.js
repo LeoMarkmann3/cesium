5b2461cd39736887d1afca97e437bbe371659981
BenediktFelsch <benedikt.felsch@student.hpi.uni-potsdam.de>
2025-12-18 09:44:20 +0100

    optimized loading tileset

--- Apps/CustomData/CustomData.js
+++ Apps/CustomData/CustomData.js
@@ -91,14 +91,11 @@ async function main() {
           dynamicScreenSpaceError: false,
           progressiveResolutionHeightFraction: 0.0,
 
-          foveatedScreenSpaceError: 0.0,
-          foveatedConeSize: 0.0,
-          foveatedMinimumScreenSpaceErrorRelaxation: 0.0,
+          foveatedScreenSpaceError: false,
 
           cullRequestsWhileMoving: false,
 
-          baseScreenSpaceError: 1024,
-          maximumScreenSpaceError: 1.0,
+          maximumScreenSpaceError: 0.0001,
 
           preloadWhenHidden: true,
           preloadFlightDestinations: true,
